b37b1d2983a81bbe44d3e88f88041b06ae23b697
husein arafat <huseinar04@gmail.com>
2025-07-03 21:05:11 +0700

    Security: Pindahkan SCRIPT_URL ke environment variables

--- config.js
+++
@@ -1,5 +0,0 @@
-// File ini TIDAK boleh di-upload ke GitHub.
-// Pastikan file ini ada di dalam .gitignore
-
-const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbwO_B73AVF7VCBlhvR_59eT22mgz7t_rUGYPYvFIfX6YVXt4fDvyRgWaz82GWP-aCH0oA/exec";

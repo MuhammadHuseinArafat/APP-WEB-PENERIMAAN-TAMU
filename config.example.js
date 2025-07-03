de8a6aae3f1c6a914a9d50914e205336b1e6550e
husein arafat <huseinar04@gmail.com>
2025-07-03 16:53:21 +0700

    aplikasi penerimaan tamu

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// File ini aman untuk di-upload ke GitHub.
+// Gunakan sebagai contoh untuk membuat file config.js Anda sendiri.
+
+const SCRIPT_URL = "MASUKKAN_URL_WEB_APP_ANDA_DARI_GOOGLE_APPS_SCRIPT";
+const PASSWORD = "MASUKKAN_KATA_SANDI_ANDA";
